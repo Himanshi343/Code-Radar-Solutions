7b07600aa82ddc46e0fbc1b7c3fa32eaa072538d
Himanshi Garg <himanshigarg2006@gmail.com>
2025-01-30 10:59:16 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,6 +3,6 @@ int main() {
     float a,b;
     char c;
     scanf("%f %f %c",&a,&b,&c);
-    (c=='+')?printf("%d",a+b):(c=='-')?printf("%d",a-b):(c=='*')?printf("%d",a*b):printf("%d",a/b);
+    (c=='+')?printf("%d",a+b):(c=='-')?printf("%d",a-b):(c=='*')?printf("%d",a*b):(c=='/')?printf("%d",a/b):printf("Error");
     return 0;
 }
