5b5006b03009186055dba85e7d3ab224fb4aae7a
Himanshi Garg <himanshigarg2006@gmail.com>
2025-04-06 14:49:27 +0530

    Solution to Find and Display the Top Scorer

--- Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
+++ Find and Display the Top Scorer@@67877d61dc107486ad5c2744/code.c
@@ -5,5 +5,9 @@ int main(){
     int num[a];
     char name[a];
     float marks[a];
-    
+    for (int i=0; i<a; i++){
+        scanf("%d",&num[i]);
+        scanf("%s",&name[i]);
+        scanf("%f",&marks[i]);
+    }
 }
