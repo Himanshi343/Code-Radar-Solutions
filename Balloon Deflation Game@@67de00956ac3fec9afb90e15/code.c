b836e5b98cd2a4b66c0abc0cdadb715032304209
Himanshi Garg <himanshigarg2006@gmail.com>
2025-04-10 00:24:04 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -1,32 +1,32 @@
-// int deflateBalloons(int air[], int n){
-//     while(1){
-//     int min = air[0];
-//         for (int i=0; i<n; i++){
-//             if (air[i]<min && min != 0){
-//                 min = air[i];
-//             }
-//         }
-//         for (int i=0; i<n; i++){
-//             air[i] -= min;
-//         }
-//         int max = air[0];
-//         for (int i=0; i<n; i++){
-//             if (air[i]>max){
-//                 max = air[i];
-//             }
+int deflateBalloons(int air[], int n){
+    while(1){
+    int min = air[0];
+        for (int i=0; i<n; i++){
+            if (air[i]<min && min != 0){
+                min = air[i];
+            }
+        }
+        for (int i=0; i<n; i++){
+            air[i] -= min;
+        }
+        int max = air[0];
+        for (int i=0; i<n; i++){
+            if (air[i]>max){
+                max = air[i];
+            }
         
-//         }
-//         printf("%d\N",max);
-//         int Z = 1;
-//         for (int i = 0; i < n; i++) {
-//             if (air[i] != 0) {
-//                 Z = 0;
-//                 break;
-//             }
-//         }
-//         if (Z) {
-//             break;
-//         }
-//     }
-//     return 0;
-// }
+        }
+        printf("%d\n",max);
+        int Z = 1;
+        for (int i = 0; i < n; i++) {
+            if (air[i] != 0) {
+                Z = 0;
+                break;
+            }
+        }
+        if (Z) {
+            break;
+        }
+    }
+    return 0;
+}
