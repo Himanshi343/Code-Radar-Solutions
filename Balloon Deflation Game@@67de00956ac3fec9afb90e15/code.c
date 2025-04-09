a1c78f155e11feaf0646a7a420cbc33b6b88e27a
Himanshi Garg <himanshigarg2006@gmail.com>
2025-04-10 00:19:47 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -1,22 +1,31 @@
 int deflateBalloons(int air[], int n){
+    while(1){
     int min = air[0];
-    for (int j=0; j<n; j++){
-
-    }
-    for (int i=0; i<n; i++){
-        if (air[i]<min){
-            min = air[i];
+        for (int i=0; i<n; i++){
+            if (air[i]<min && min != 0){
+                min = air[i];
+            }
         }
-    }
-    for (int i=0; i<n; i++){
-        air[i] -= min;
-    }
-    int max = air[0];
-    for (int i=0; i<n; i++){
-        if (air[i]>max){
-            max = air[i];
+        for (int i=0; i<n; i++){
+            air[i] -= min;
         }
+        int max = air[0];
+        for (int i=0; i<n; i++){
+            if (air[i]>max){
+                max = air[i];
+            }
         
+        }
+        printf("%d ",max);
+        int allZero = 1; // Assume all elements are zero
+        for (int i = 0; i < n; i++) {
+            if (air[i] != 0) {
+                allZero = 0; // Found a non-zero element
+                break;
+            }
+        }
+        if (allZero) {
+            break; // Exit the loop if all elements are zero
+        }
     }
-    printf("%d ",max);
 }
