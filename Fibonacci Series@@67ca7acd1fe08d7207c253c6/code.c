9efeee1186d986438af684d7c105de473509f499
Himanshi Garg <himanshigarg2006@gmail.com>
2025-04-09 23:49:29 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,30 +1,34 @@
-// int fibonacciSeries(int a){
-//     int n1 = 0;
-//     int n2 = 1;
-//     int n;
-//     printf("0 1 ");
-//     for (int i=1; i<a-1; i++){
-//         n = n1 + n2;
-//         printf("%d ",n);
-//         n1 = n2;
-//         n2 = n;
-//     }
-//     return 0;
-// }
-
-
-void fibonacciSeries(int a) {
-    int n1 = 0, n2 = 1, n;
-    if (a == 1) {
+int fibonacciSeries(int a){
+    int n1 = 0;
+    int n2 = 1;
+    int n;
+    if (a==1){
         return 0;
     }
-
     printf("0 1 ");
-    for (int i = 2; i < a; i++) { // Start from 2 since 0 and 1 are already printed
+    for (int i=1; i<a-1; i++){
         n = n1 + n2;
-        printf("%d ", n);
+        printf("%d ",n);
         n1 = n2;
         n2 = n;
     }
-    printf("\n");
+    return 0;
 }
+
+
+// void fibonacciSeries(int a) {
+//     int n1 = 0, n2 = 1, n;
+//     if (a == 1) {
+//         printf("0\n");
+//         return;
+//     }
+
+//     printf("0 1 ");
+//     for (int i = 2; i < a; i++) {
+//         n = n1 + n2;
+//         printf("%d ", n);
+//         n1 = n2;
+//         n2 = n;
+//     }
+//     printf("\n");
+// }
