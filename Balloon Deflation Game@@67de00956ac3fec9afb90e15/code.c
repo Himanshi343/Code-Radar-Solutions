a3f7a55bf2b4b5710ed4831a12d15635175e2248
Himanshi Garg <himanshigarg2006@gmail.com>
2025-04-10 10:27:45 +0530

    Solution to Balloon Deflation Game

--- Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
+++ Balloon Deflation Game@@67de00956ac3fec9afb90e15/code.c
@@ -1,31 +1,70 @@
-int deflateBalloons(int air[], int n){
-    int min = 1;
-        for (int i=0; i<n; i++){
-            if (air[i]<min && min != 0){
-                min = air[i];
-            }
-        }
-        for (int i=0; i<n; i++){
-            air[i] -= min;
-        }
-        int max = air[0];
-        for (int i=0; i<n; i++){
-            if (air[i]>max){
-                max = air[i];
-            }
+// int deflateBalloons(int air[], int n){
+//     int min = 1;
+//         for (int i=0; i<n; i++){
+//             if (air[i]<min && min != 0){
+//                 min = air[i];
+//             }
+//         }
+//         for (int i=0; i<n; i++){
+//             air[i] -= min;
+//         }
+//         int max = air[0];
+//         for (int i=0; i<n; i++){
+//             if (air[i]>max){
+//                 max = air[i];
+//             }
         
-        }
-        printf("%d\n",max);
-        int Z = 1;
-        for (int i = 0; i < n; i++) {
-            if (air[i] != 0) {
-                Z = 0;
-                break;
-            }
-        }
-        if (Z) {
-            return 0;
-        }
+//         }
+//         printf("%d\n",max);
+//         int Z = 1;
+//         for (int i = 0; i < n; i++) {
+//             if (air[i] != 0) {
+//                 Z = 0;
+//                 break;
+//             }
+//         }
+//         if (Z) {
+//             return 0;
+//         }
     
-    deflateBalloons(air, n);
+//     deflateBalloons(air, n);
+// }
+
+int deflateBalloons(int air[], int n) {
+    int min = INT_MAX; // Set min to the maximum possible integer initially
+    for (int i = 0; i < n; i++) {
+        if (air[i] > 0 && air[i] < min) { // Only consider positive values
+            min = air[i];
+        }
+    }
+
+    if (min == INT_MAX) { // If no positive value is found, all balloons are deflated
+        return 0;
+    }
+
+    for (int i = 0; i < n; i++) {
+        air[i] -= min;
+    }
+
+    int max = air[0];
+    for (int i = 0; i < n; i++) {
+        if (air[i] > max) {
+            max = air[i];
+        }
+    }
+
+    printf("%d\n", max);
+
+    int Z = 1;
+    for (int i = 0; i < n; i++) {
+        if (air[i] != 0) {
+            Z = 0;
+            break;
+        }
+    }
+    if (Z) {
+        return 0; // Base case: all balloons are deflated
+    }
+
+    return deflateBalloons(air, n); // Recursive call, return the result
 }
